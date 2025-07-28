a61e0c6cf947ec6f8dd7289ffb9ff3249697e5bc
Nolan Reilly <nolanreilly02@gmail.com>
2025-07-27 20:15:35 -0500

    Create Kittens Model and Controllers

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,23 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
