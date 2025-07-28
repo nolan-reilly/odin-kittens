73057bc803ba2bbf3c2a6209e741904295814976
Nolan Reilly <nolanreilly02@gmail.com>
2025-07-27 20:40:41 -0500

    Make Kitten RESTful Functionality

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,20 +4,46 @@ class KittensController < ApplicationController
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_path, notice: "Kitten was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "Kitten updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
